2b01e1a051c18741a859c006254b55e253f2bf83
anudanethmin <anudanethmin@hotmail.com>
2026-07-20 18:40:15 +0100

    Fix fullscreen in movie/TV player by expanding iframe permissions

--- app.js
+++ app.js
@@ -566,7 +566,7 @@
                             <!-- Servers injected here -->
                         </div>
                         <div class="video-container">
-                            <iframe id="inline-video-iframe" src="" frameborder="0" allowfullscreen allow="autoplay; fullscreen"></iframe>
+                            <iframe id="inline-video-iframe" src="" frameborder="0" allow="fullscreen; autoplay; encrypted-media; picture-in-picture" allowfullscreen="true"></iframe>
                         </div>
                         <div class="player-footer">
                             <div class="streaming-via">Streaming via <span id="current-server-name">VidLink</span>. If playback fails or shows ads, try a different server above.</div>
