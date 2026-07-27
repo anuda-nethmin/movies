f9ab1a2a0355bfbf942025a8bf50b3fd23cd79da
anudanethmin <anudanethmin@hotmail.com>
2026-07-27 19:14:04 +0100

    feat: add vidcore and vidsrc pro as fallback streaming options

--- app.js
+++ app.js
@@ -1532,6 +1532,8 @@
         { name: 'SmashyStream', getMovie: id => `https://embed.smashystream.com/playere.php?tmdb=${id}`, getTv: (id, s, e) => `https://embed.smashystream.com/playere.php?tmdb=${id}&season=${s}&ep=${e}` },
         { name: '2Embed', getMovie: id => `https://www.2embed.cc/embed/${id}`, getTv: (id, s, e) => `https://www.2embed.cc/embedtv/${id}&s=${s}&e=${e}` },
         { name: 'MultiEmbed', getMovie: id => `https://multiembed.mov/?video_id=${id}&tmdb=1`, getTv: (id, s, e) => `https://multiembed.mov/?video_id=${id}&tmdb=1&s=${s}&e=${e}` },
+        { name: 'VidCore', getMovie: id => `https://vidcore.org/embed/movie?tmdb=${id}`, getTv: (id, s, e) => `https://vidcore.org/embed/tv?tmdb=${id}&season=${s}&episode=${e}` },
+        { name: 'VidSrc PRO', getMovie: id => `https://vidsrc.pro/embed/movie/${id}`, getTv: (id, s, e) => `https://vidsrc.pro/embed/tv/${id}/${s}/${e}` },
         { name: 'Anime (AutoEmbed)', getMovie: id => `https://autoembed.to/movie/tmdb/${id}`, getTv: (id, s, e) => `https://autoembed.to/tv/tmdb/${id}-${s}-${e}` },
         { name: 'Anime (Embed.su)', getMovie: id => `https://embed.su/embed/movie/${id}`, getTv: (id, s, e) => `https://embed.su/embed/tv/${id}/${s}/${e}` },
         { name: 'Anime (NontonGo)', getMovie: id => `https://nontongo.win/embed/movie/${id}`, getTv: (id, s, e) => `https://nontongo.win/embed/tv/${id}/${s}/${e}` }
