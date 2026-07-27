31366c973e47f5d7d8b9d82d7c464d5ffe47837e
anudanethmin <anudanethmin@hotmail.com>
2026-07-27 19:13:25 +0100

    feat: add embed.su and nontongo as dedicated anime proxy options

--- app.js
+++ app.js
@@ -1532,7 +1532,9 @@
         { name: 'SmashyStream', getMovie: id => `https://embed.smashystream.com/playere.php?tmdb=${id}`, getTv: (id, s, e) => `https://embed.smashystream.com/playere.php?tmdb=${id}&season=${s}&ep=${e}` },
         { name: '2Embed', getMovie: id => `https://www.2embed.cc/embed/${id}`, getTv: (id, s, e) => `https://www.2embed.cc/embedtv/${id}&s=${s}&e=${e}` },
         { name: 'MultiEmbed', getMovie: id => `https://multiembed.mov/?video_id=${id}&tmdb=1`, getTv: (id, s, e) => `https://multiembed.mov/?video_id=${id}&tmdb=1&s=${s}&e=${e}` },
-        { name: 'Anime (AutoEmbed)', getMovie: id => `https://autoembed.to/movie/tmdb/${id}`, getTv: (id, s, e) => `https://autoembed.to/tv/tmdb/${id}-${s}-${e}` }
+        { name: 'Anime (AutoEmbed)', getMovie: id => `https://autoembed.to/movie/tmdb/${id}`, getTv: (id, s, e) => `https://autoembed.to/tv/tmdb/${id}-${s}-${e}` },
+        { name: 'Anime (Embed.su)', getMovie: id => `https://embed.su/embed/movie/${id}`, getTv: (id, s, e) => `https://embed.su/embed/tv/${id}/${s}/${e}` },
+        { name: 'Anime (NontonGo)', getMovie: id => `https://nontongo.win/embed/movie/${id}`, getTv: (id, s, e) => `https://nontongo.win/embed/tv/${id}/${s}/${e}` }
     ];
 
     let currentVideoState = { id: null, type: null, serverIndex: 0, season: 1, episode: 1, episodesData: [] };
