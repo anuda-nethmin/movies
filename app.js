2f85cf35f1a46923f11f567bb93ae89ec5e4ef9a
anudanethmin <anudanethmin@hotmail.com>
2026-07-31 15:08:38 +0100

    feat: re-add standard VidSrc endpoint

--- app.js
+++ app.js
@@ -1528,6 +1528,7 @@
     const SERVERS = [
         { name: 'VidLink', getMovie: id => `https://vidlink.pro/movie/${id}?primaryColor=38bdf8&autoplay=false`, getTv: (id, s, e) => `https://vidlink.pro/tv/${id}/${s}/${e}?primaryColor=38bdf8&autoplay=false` },
         { name: 'Videasy', getMovie: id => `https://player.videasy.net/movie/${id}`, getTv: (id, s, e) => `https://player.videasy.net/tv/${id}/${s}/${e}` },
+        { name: 'VidSrc', getMovie: id => `https://vidsrc.net/embed/movie?tmdb=${id}`, getTv: (id, s, e) => `https://vidsrc.net/embed/tv?tmdb=${id}&season=${s}&episode=${e}` },
         { name: 'MultiEmbed', getMovie: id => `https://multiembed.mov/?video_id=${id}&tmdb=1`, getTv: (id, s, e) => `https://multiembed.mov/?video_id=${id}&tmdb=1&s=${s}&e=${e}` },
         { name: 'Anime (VidSrc)', getMovie: id => `https://vidsrc.icu/embed/movie/${id}`, getTv: (id, s, e) => `https://vidsrc.icu/embed/tv/${id}/${s}/${e}` }
     ];
